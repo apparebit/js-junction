52d320cce2691a8563c96c3f2c3d7c20a8fc73f7
Robert Grimm <grimmr@users.noreply.github.com>
2017-12-20 21:32:47 -0500

    oddjob: add support for processing key-value pairs
    * Add isPropertyKey() and getOwnPropertyKeys() to inspect objects.
    * Add toKeyValue() and withKeyValue() to support more convenient APIs
    ** Use object literals with property shorthand
    * Expose all four and isObject() through objects.js

---
+++ packages/oddjob/internal/key-value.js
@@ -0,0 +1,73 @@
+/* (C) Copyright 2017 Robert Grimm */
+
+import { InvalidArgValue } from './errors';
+import { default as isObject } from './is-object';
+
+const {
+  getOwnPropertyNames,
+  getOwnPropertySymbols,
+} = Object;
+
+const { isArray } = Array;
+
+const KEY_TYPES = new Set(['number', 'string', 'symbol']);
+
+export function isPropertyKey(value) {
+  return KEY_TYPES.has(typeof value);
+}
+
+export function getOwnPropertyKeys(object) {
+  return [
+    ...getOwnPropertyNames(object),
+    ...getOwnPropertySymbols(object)
+  ];
+}
+
+/**
+ * Convert the object to a `[key, value]` pair. The argument must, in fact, be
+ * an object.
+ */
+export function toKeyValue(object) {
+  if( isArray(object) ) {
+    if( object.length === 2 && isPropertyKey(object[0]) ) {
+      return object;
+    }
+  } else {
+    const keys = getOwnPropertyKeys(object);
+
+    if( keys.length === 1 ) {
+      const [key] = keys;
+      return [key, object[key]];
+    } else if(
+      keys.length === 2
+      && keys.includes('key')
+      && keys.includes('value')
+    ) {
+      return [object.key, object.value];
+    }
+  }
+
+  throw InvalidArgValue(
+    'object', object, 'an object representing a single key-value pair');
+}
+
+export function withKeyValue(original, ...indices) {
+  // Use a proxy instead of a function calling a function. The proxy not only
+  // traps function calls, but also transparently exposes the wrapped function's
+  // properties, including `length` and `name`. #Winning!
+  if( !indices.length ) indices = [0];
+
+  return new Proxy(original, { apply(target, that, args) {
+    for(const index of indices) {
+      const arg = args[index];
+
+      if( isObject(arg) ) {
+        const [key, value] = toKeyValue(arg);
+        args[index] = key;
+        args.splice(index + 1, 0, value);
+      }
+    }
+
+    return Reflect.apply(target, that, args);
+  }});
+}
---
+++ packages/oddjob/objects.js
@@ -0,0 +1,12 @@
+/* (C) Copyright 2017 Robert Grimm */
+
+export {
+  default as isObject
+} from './internal/is-object';
+
+export {
+  isPropertyKey,
+  getOwnPropertyKeys,
+  toKeyValue,
+  withKeyValue,
+} from './internal/key-value';
