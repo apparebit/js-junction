0f9502aa00a642c7009d4e79c1a4920519e989f0
Robert Grimm <grimmr@users.noreply.github.com>
2017-12-20 21:33:24 -0500

    oddjob: improve error constructors with key-value shorthand

--- packages/oddjob/errors.js
+++ packages/oddjob/errors.js
@@ -6,21 +6,26 @@ import {
   InvalidArgTypeMsg,
   InvalidArgValueMsg,
 } from './internal/errors';
+
+import {
+  withKeyValue,
+} from './internal/key-value';
+
 import show from './show';
 
 const showCount = show().length().noun('element');
 const showArgNames = show().quoted.elements().noun('argument').verb();
 
 export const DuplicateBinding = E('ERR_DUPLICATE_BINDING',
-  (key, value, replacement) =>
-    `"${key}" is bound to "${value}", cannot be rebound to "${replacement}"`);
+  withKeyValue((key, value, replacement) =>
+    `"${key}" is bound to "${value}", cannot be rebound to "${replacement}"`));
 export const InvalidArgType = E('ERR_INVALID_ARG_TYPE',
-  InvalidArgTypeMsg, CodedTypeError);
+  withKeyValue(InvalidArgTypeMsg), CodedTypeError);
 export const InvalidArgValue = E('ERR_INVALID_ARG_VALUE',
-  InvalidArgValueMsg);
+  withKeyValue(InvalidArgValueMsg));
 export const InvalidArrayLength = E('ERR_INVALID_ARRAY_LENGTH',
-  (key, value, expected) =>
-    `array "${key}" has ${showCount.of(value)}, but should have ${expected}`);
+  withKeyValue((key, value, expected) =>
+    `array "${key}" has ${showCount.of(value)}, but should have ${expected}`));
 export const MethodNotImplemented = E('ERR_METHOD_NOT_IMPLEMENTED',
   name => `the ${name}() method is not implemented`);
 export const MissingArgs = E('ERR_MISSING_ARGS',
