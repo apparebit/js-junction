26582bc5ac4f94de392edac6f2c617b68280f5cb
Robert Grimm <grimmr@users.noreply.github.com>
2017-12-23 23:17:10 -0500

    proact: update tests
    To reflect modified functionality and to fully cover new functionality

--- tests/proact.test.js
+++ tests/proact.test.js
@@ -1,37 +1,40 @@
 /* (c) Copyright 2017 Robert Grimm */
 
-import { toSymbolKey } from '@grr/oddjob';
-
-import Tag from '@grr/proact/util/tag';
+import Tag from '@grr/proact/model/tag';
 import typeAttribute from '@grr/proact/model/attributes';
 import typeElement from '@grr/proact/model/elements';
 
 import {
   ComponentBase,
-  isComponent,
   RenderFunction,
   toComponent,
 } from '@grr/proact/component';
 
+import isComponent from '@grr/proact/component/is-component';
+import { define, lookup } from '@grr/proact/component/registry';
+
 import {
-  define,
-  lookup,
-} from '@grr/proact/component/registry';
+  ElementBase,
+  StandardElement,
+  CustomElement,
+} from '@grr/proact/element';
 
 import harness from './harness';
 
 const { toStringTag } = Symbol;
-const { HTML, Proact } = Tag;
+const { HTML } = Tag;
 
 const CODE_DUPLICATE_BINDING = { code: 'ERR_DUPLICATE_BINDING' };
 const CODE_INVALID_ARG_TYPE = { code: 'ERR_INVALID_ARG_TYPE' };
 const CODE_INVALID_ARG_VALUE = { code: 'ERR_INVALID_ARG_VALUE' };
 const CODE_METHOD_NOT_IMPLEMENTED = { code: 'ERR_METHOD_NOT_IMPLEMENTED' };
-const TAG_FUNCTIONAL_COMPONENT = toSymbolKey(Proact.Component.Functional);
+const TAG_FUNCTIONAL_COMPONENT = 'Proact.Component.Functional';
+const TAG_STANDARD_ELEMENT = 'Proact.Element.Standard';
+const TAG_CUSTOM_ELEMENT = 'Proact.Element.Custom';
 
 // -----------------------------------------------------------------------------
 
-harness.test('@grr/oddjob', t => {
+harness.test('@grr/proact', t => {
   t.test('model', t => {
     t.test('.typeAttribute()', t => {
       t.is(typeAttribute('aria-disabled'), HTML.Attribute.TrueFalse);
@@ -65,6 +68,7 @@ harness.test('@grr/oddjob', t => {
     const c3 = new RenderFunction(fn, 'renderer');
 
     t.test('ComponentBase', t => {
+      t.is(c1.name, 'abstract');
       t.throws(() => c1.render(), CODE_METHOD_NOT_IMPLEMENTED);
       t.throws(() => c1[toStringTag], CODE_METHOD_NOT_IMPLEMENTED);
       t.end();
@@ -75,6 +79,8 @@ harness.test('@grr/oddjob', t => {
       t.is(c3[toStringTag], TAG_FUNCTIONAL_COMPONENT);
       t.is(c2.name, 'fn');
       t.is(c3.name, 'renderer');
+
+      t.throws(() => new RenderFunction('boo'));
       t.end();
     });
 
@@ -116,6 +122,54 @@ harness.test('@grr/oddjob', t => {
     t.throws(() => define(renderer), CODE_DUPLICATE_BINDING);
 
     t.is(lookup('renderer').render, renderer);
+    t.end();
+  });
+
+  // ---------------------------------------------------------------------------
+
+  t.test('element', t => {
+    t.test('ElementBase', t => {
+      t.throws(() => new ElementBase()[toStringTag]);
+      t.end();
+    });
+
+    t.test('StandardElement', t => {
+      const span = new StandardElement('span', null, ['text']);
+
+      t.is(span[toStringTag], TAG_STANDARD_ELEMENT);
+      t.is(span.name, 'span');
+      t.same(span.attributes, {});
+      t.same(span.children, ['text']);
+      t.is(span.component, void 0);
+      t.notOk(span.isCustom());
+
+      const br = new StandardElement('br');
+
+      t.is(br[toStringTag], TAG_STANDARD_ELEMENT);
+      t.is(br.name, 'br');
+      t.same(br.attributes, {});
+      t.same(br.children, []);
+      t.is(br.component, void 0);
+      t.notOk(br.isCustom());
+
+      t.end();
+    });
+
+    t.test('CustomElement', t => {
+      const rf = new RenderFunction(function custom() {});
+      const custom = new CustomElement(rf, null, 'text');
+
+      t.is(custom[toStringTag], TAG_CUSTOM_ELEMENT);
+      t.is(custom.name, 'custom');
+      t.same(custom.attributes, {});
+      t.same(custom.children, ['text']);
+      t.is(custom.component, rf);
+      t.ok(custom.isCustom());
+
+      t.throws(() => new CustomElement('br'));
+      t.end();
+    });
+
     t.end();
   });
 
