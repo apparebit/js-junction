f68da45e3c3a95e7f8cac1195da1131b57e76be5
Robert Grimm <grimmr@users.noreply.github.com>
2018-01-03 04:51:30 -0500

    chore: improve documentation inside node/is-node.js

--- packages/proact/node/is-node.js
+++ packages/proact/node/is-node.js
@@ -9,10 +9,10 @@ export default function isDocumentNode(value) {
   if( value == null || typeof value !== 'object' ) {
     return false;
   } else if( isDocumentNodeTag(value[toStringTag]) ) {
-    // Fast path checks for Proact's own nodes.
+    // The fast path checks for Proact's node tags and may be wrong.
     return true;
   } else {
-    // General path checks for core vDOM properties.
+    // The slow path checks for node properties and is more precise.
     return typeof value.name === 'string'
       && typeof value.attributes === 'object'
       && isArray(value.children)
