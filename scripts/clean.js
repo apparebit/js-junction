7881507c973cf65f8343c369b1e1db6a927d577b
Robert Grimm <grimmr@users.noreply.github.com>
2018-03-10 19:10:46 -0500

    chore: factor repository clean-up into script

---
+++ scripts/clean.js
@@ -0,0 +1,16 @@
+/* (c) Copyright 2018 Robert Grimm */
+
+import rimraf from 'rimraf';
+import util from 'util';
+
+const doRemove = util.promisify(rimraf);
+const remove = pattern => doRemove(pattern, {});
+
+(async function run() {
+  await Promise.all([
+    '.nyc_output',
+    'coverage',
+    'yarn-error.log',
+    '**/node_modules/.cache',
+  ].map(remove));
+})();
