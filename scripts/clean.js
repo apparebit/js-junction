158f1abbf0345b89da368129867543e6bcb8ba2b
Robert Grimm <apparebit@users.noreply.github.com>
2018-05-14 03:36:27 -0400

    tests: rename clean-without-esm.js to clean.js again, add --deep cleaning

---
+++ scripts/clean.js
@@ -0,0 +1,57 @@
+/* (c) Copyright 2018 Robert Grimm */
+
+// Do not even think of using esm, since it writes metadata to its cache at
+// `node_modules/.cache/esm` on process exit. That is somewhat less than optimal
+// for a script, which seeks to delete all such caches.
+
+const doReadDirectory = require('fs').readdir;
+const { resolve } = require('path');
+const rimraf = require('rimraf');
+const { promisify } = require('util');
+
+const all = Promise.all.bind(Promise);
+const { argv } = process;
+const doRemove = promisify(rimraf);
+const readDirectory = promisify(doReadDirectory);
+const remove = pattern => doRemove(pattern, {});
+const deep = argv.includes('--deep');
+
+async function removeNamedDirectories() {
+  let patterns;
+  if( !deep ) {
+    patterns = ['**/node_modules/.cache/esm'];
+  } else {
+    patterns = [
+      '.nyc_output',
+      'coverage',
+      'yarn-error.log',
+      '**/node_modules/.cache'
+    ];
+  }
+
+  await all(patterns.map(pattern => remove(resolve(__dirname, '..', pattern))));
+}
+
+async function removeEmptyDirectories() {
+  // Sort the directories from longest path to shortest path. That way, we
+  // always check children before their parents.
+
+  // eslint-disable-next-line global-require
+  const paths = (await require('fast-glob')('packages/*/node_modules/**/', {
+    absolute: true,
+    cwd: resolve(__dirname, '..'),
+    dot: true,
+    onlyDirectories: true,
+    onlyFiles: false,
+    unique: true,
+  })).sort((a, b) => b.length - a.length);
+
+  for( const path of paths ) {
+    if( (await readDirectory(path)).length === 0 ) {
+      await remove(path);
+    }
+  }
+}
+
+removeNamedDirectories();
+if( deep ) removeEmptyDirectories();
