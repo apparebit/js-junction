42b1d358ea69203dda38343713582381958bbe87
Robert Grimm <apparebit@users.noreply.github.com>
2018-05-16 10:14:03 -0400

    scripts: introduce new script for setting the version of dependencies
    mostly, this helps avoid updating a growing number of manifests by hand

---
+++ scripts/version.js
@@ -0,0 +1,33 @@
+/* (c) Copyright 2018 Robert Grimm */
+
+import chalk from 'chalk';
+import { format } from 'util';
+import parseArguments from 'mri';
+import { updateDependency } from '@grr/inventory';
+
+const args = parseArguments(process.argv.slice(2), {
+  boolean: ['debug'],
+});
+
+console.dir(args);
+
+if( args._.length !== 2 ) {
+  console.error(chalk.red(`Usage: yarn run version [--debug] <package> <version>`));
+  process.exit(13); // eslint-disable-line no-process-exit
+}
+
+const [name, version] = args._;
+if( !/^\d+\.\d+.\d+$/u.test(version) ) {
+  console.error(chalk.red(`Package version "${version}" does not have format "\\d+.\\d+.\\d+"`));
+  process.exit(13); // eslint-disable-line no-process-exit
+}
+
+if( args.debug ) {
+  updateDependency(name, version, {
+    logger(...args) {
+      console.error(chalk.grey(`# ${format(...args)}`));
+    }
+  });
+} else {
+  updateDependency(name, version);
+}
